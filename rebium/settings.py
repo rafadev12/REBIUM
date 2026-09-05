6cb5c645a421c0d459b13cb9c93ae64e30bc4e7b
Rafael Gonzalez <rafadev12>
2026-09-04 22:36:15 -0400

    Configuracion de settings y requisitos para Render

--- rebium/settings.py
+++ rebium/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import dj_database_url
 
 # Application definition
 
